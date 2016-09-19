8a0c4748e507933e901e43cbfa4a20108096b47f
Breuzon Camille <breuzon@a13p7.fil.univ-lille1.fr>
2016-09-19 18:19:16 +0200

    try.h try.c asm typedef

--- try.c
+++ try.c
@@ -1,15 +1,24 @@
+#include "try.h"
+#include <assert.h>
+
 int try(struct ctx_s *pctx, func_t *f, int arg) {
 	pctx -> ctx_magic = CTX_MAGIC;
-	asm(movl %esp ~> pctx->esp;
-	asm(movl %ebp ~> pctx->ebp;
+	asm("movl %%esp, %0"
+	    : "=r" (pctx->ctx_esp));
+	asm("movl %%ebp, %0"
+	    : "=r" (pctx->ctx_ebp));
 	return f(arg);
 }
 
-int throw(struct ctx_s pctx, int val) {
+int throw(struct ctx_s *pctx, int val) {
 	static int r = 0;
 	assert(pctx->ctx_magic == CTX_MAGIC);
 	r = val;
-	assert(movl pctx->esp ~> %esp);
-	assert(movl pctx->ebp ~> %ebp);
+	asm("movl %0, %%esp"
+	    : 
+	    : "r" (pctx->ctx_esp));
+	asm("movl %0, %%ebp"
+	    :
+	    : "r" (pctx->ctx_ebp));
 	return r;
 }
--- try.h
+++ try.h
@@ -1,7 +1,9 @@
 #define CTX_MAGIC 0xABBACAFE
 
+typedef int (func_t) (int);
+
 struct ctx_s {
 	void * ctx_esp;
 	void * ctx_ebp;
 	unsigned ctx_magic;
-}
+};
